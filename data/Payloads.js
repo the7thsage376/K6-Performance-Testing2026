25ebe14aac9aae537212b3fb6c1d6fbfe54b943d
Lerato Moloi <moloil405@gmail.com>
2026-05-03 19:48:49 +0200

    Added payloads and requests

---
+++ data/Payloads.js
@@ -0,0 +1,26 @@
+export const PAYLAODS ={
+
+    login: {
+  "email": "testing007@gmail.com",
+  "password": "@password1234"
+},
+
+post_testimonial: {
+  "title": "Bootcamp review",
+  "content": "Ndosi is the best!",
+  "rating": 5,
+  "isPublic": true
+},
+
+update_testimonial: {
+  "title": "Bootcamp review revised",
+  "content": "Ndosi is the greatest!",
+  "rating": 5
+},
+
+delete_testimonial: {
+  "success": true,
+  "message": "Testimonial deleted successfully"
+}
+
+};
---
+++ requests/authRequest.js
@@ -0,0 +1,2 @@
+import http from 'k6/http';
+import { URLs } from '../config/urls';
