0ce08302c164130a6ff6c26ea4618cdefe43fc45
Lerato Moloi <moloil405@gmail.com>
2026-09-11 18:52:30 +0200

    Finished pipeline and added reporting

--- tests/06_testSuite.js
+++ tests/06_testSuite.js
@@ -4,6 +4,11 @@ import postTestimonialTest from './03_postTestimonialTest.js';
 import updateTestimonialTest from './04_updateTestimonialTest.js';
 import deleteTestimonialTest from './05_deleteTestimonialTest.js';
 
+// Html report generation for k6 tests
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
+
+
 export default function () {
     
     loginTest();
@@ -11,4 +16,11 @@ export default function () {
     postTestimonialTest();
     updateTestimonialTest();
     deleteTestimonialTest();
+}
+
+export function handleSummary(data) {
+    return {
+        "report.html": htmlReport(data),
+        stdout: textSummary(data, { indent: " ", enableColors: true }),
+    };
 }
